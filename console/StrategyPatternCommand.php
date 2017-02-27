f9d1ddde68712ab9ef4230c281c153f9865e41d4
mireia <mireia@actualweb.es>
2017-02-27 09:11:15 +0100

    Aplicados los cambios de la clase Command en StrategyPatternCommand

--- console/StrategyPatternCommand.php
+++ console/StrategyPatternCommand.php
@@ -7,12 +7,9 @@ use HFDP\strategy\DuckMallard;
 use HFDP\strategy\DuckModel;
 use HFDP\strategy\FlayNoWay;
 use HFDP\strategy\QuackSqueak;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
-class StrategyPatternCommand extends Command
+
+class StrategyPatternCommand extends \HFDPConsole\Command
 {
     protected function configure()
     {
@@ -22,36 +19,35 @@ class StrategyPatternCommand extends Command
             ->setHelp('Strategy Pattern: Duck example');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    public function fire()
     {
-       $output->writeln('<info>Strategy Pattern</info>');
-       $output->writeln('');
-        $this->duckModel($output);
-        $output->writeln('');
-        $this->duckMallard($output);
-
-
+        $this->info('Strategy Pattern');
+        $this->output->writeln('');
+        $this->duckModel();
+        $this->output->writeln('');
+        $this->duckMallard();
     }
     
-    protected function duckModel(OutputInterface $output){
+    protected function duckModel(){
         $duckModel = new DuckModel();
-        $output->writeln('<comment>'.$duckModel->display().'</comment>');
-        $output->writeln($duckModel->performFly());
-        $output->writeln($duckModel->performQuack());
+        $this->comment($duckModel->display());
+        $this->output->writeln($duckModel->performFly());
+        $this->output->writeln($duckModel->performQuack());
     }
 
-    protected function duckMallard(OutputInterface $output)
+    protected function duckMallard()
     {
         $mallardDuck = new DuckMallard();
-
-        $output->writeln('<comment>'.$mallardDuck->display().'</comment>');
-        $output->writeln($mallardDuck->performFly());
-        $output->writeln($mallardDuck->performQuack());
-        $output->writeln('<options=bold,underscore>Alterando comportamiento</>');
+        $this->comment($mallardDuck->display());
+        $this->output->writeln($mallardDuck->performFly());
+        $this->output->writeln($mallardDuck->performQuack());
+        $this->underline('Alterando comportamiento');
         $mallardDuck->setFlyBehavior(new FlayNoWay());
-        $output->writeln($mallardDuck->performFly());
+        $this->output->writeln($mallardDuck->performFly());
         $mallardDuck->setQuackBehavior(new QuackSqueak());
-        $output->writeln($mallardDuck->performQuack());
+        $this->output->writeln($mallardDuck->performQuack());
 
     }
+
+
 }
